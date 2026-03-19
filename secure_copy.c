68ffb197d43cf455ac205d5880dc3cb3b97f578d
Timofey Chernyshev <timkachern@mail.ru>
2026-03-19 12:14:45 +0300

    feat: implement worker pool pattern to proccess multiple files

--- secure_copy.c
+++ secure_copy.c
@@ -1,119 +1,161 @@
+// 3 Обновка /secure_copy file1.txt file2.txt ... fileN.txt /dir key
+// 3 потока (1 поток - чтение/запись/зашифрование)
+// в /dir зашифрованные файлы, которые имеют то же название что и исходник
+// в файл log.txt пишем логи туда же откуда запускаем код 
+// время, имя файла, ID процесса
+// trylock или timelock (5 сек, если > то выводим предупреждение и завершаем)
+// 1) файлы копируются
+// 2) на C
+// 3) нет дубликатов (потоки не работают с одними и теми же файлами)
+// 4) файлы корректно расшифровываются
+
 #include <string.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <libgen.h>
+#include <errno.h>
 #include "caesar.h"
 #include "secure_copy.h"
 
-volatile sig_atomic_t keep_running = 1;
+void* worker(void* arg) {
+    thread_args_t* a = arg;
 
-static void sigint_handler(int sig) {
-    (void)sig;
-    keep_running = 0;
-}
+    while (1) {
+        char* filename;
 
-void* writer_thread(void* arg) {
-    thread_args_t* args = (thread_args_t*)arg;
-    buffer_t *buffer;
-    char processed[BUF_SIZE];
+        struct timespec ts;
+        clock_gettime(CLOCK_REALTIME, &ts);
+        ts.tv_sec += 5;
 
-    while (keep_running || get_size(args->q) > 0) {
-        if (get_size(args->q) == 0) {
-            sched_yield();
-            continue;
+        if (pthread_mutex_timedlock(&a->file_index_mutex, &ts) == ETIMEDOUT) {
+            printf("Warning: mutex timeout\n");
+            exit(1);
         }
-        
-        dequeue(args->q, &buffer);
-        caesar(buffer->data, processed, buffer->len);
-        fwrite(processed, 1, buffer->len, args->dst_file);
-        
-        if (buffer->is_last) {
-            printf("\n");
-            free(buffer);
-            break; 
-        }
-        free(buffer);
-    }
 
-    return NULL;
-}
-
-void* reader_thread(void* arg) {
-    thread_args_t *args = (thread_args_t*) arg;
-
-    while (keep_running) {
-        buffer_t *buffer = malloc(sizeof(buffer_t));
-        buffer->len = fread(
-            buffer->data,
-            1,
-            sizeof(buffer->data),
-            args->src_file
-        );
-
-        buffer->is_last = (buffer->len < sizeof(buffer->data));
-
-        enqueue(args->q, &buffer);
-
-        if (buffer->is_last) {
+        if (a->next_file_index >= a->total_files) {
+            pthread_mutex_unlock(&a->file_index_mutex);
             break;
         }
+
+        filename = a->filenames[a->next_file_index];
+        a->next_file_index++;
+
+        pthread_mutex_unlock(&a->file_index_mutex);
+
+        int status = process_file(filename, a->out_dir);
+
+        pthread_mutex_lock(&a->counter_mutex);
+        a->completed_files++;
+        pthread_mutex_unlock(&a->counter_mutex);
+
+        pthread_mutex_lock(&a->log_mutex);
+        log_write(a->log, filename, status);
+        pthread_mutex_unlock(&a->log_mutex);
     }
 
     return NULL;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 4) {
-        fprintf(stderr, "Usage: %s <src_path> <dst_path> <key>\n", argv[0]);
-        return 1;
-    }
-
-    char* src_path = argv[1];
-    char* dst_path = argv[2];
-    char* key = argv[3];
-
-    signal(SIGINT, sigint_handler);
-
-    caesar_key(key[0]);
-
-    queue* q = create_queue(sizeof(buffer_t*));
-    q->mutex = malloc(sizeof(pthread_mutex_t));
-    pthread_mutex_init(q->mutex, NULL);
-
-    FILE* src = fopen(src_path, "rb");
+int process_file(char* filename, char* out_dir) {
+    FILE* src = fopen(filename, "rb");
     if (!src) {
-        perror("cannot open src");
-        destroy_queue(q);
-        return 1;
+        perror("fopen src");
+        return -1;
     }
 
-    FILE* dst = fopen(dst_path, "wb");
+    char out_path[512];
+
+    char* base = basename(filename);
+    snprintf(out_path, sizeof(out_path), "%s/%s", out_dir, base);
+
+    FILE* dst = fopen(out_path, "wb");
     if (!dst) {
-        perror("cannot open dst");
+        perror("fopen dst");
         fclose(src);
-        destroy_queue(q);
-        return 1;
+        return -1;
     }
 
-    thread_args_t args = {
-        .src_file = src,
-        .dst_file = dst,
-        .q = q
-    };
-    pthread_t thread_reader, thread_writer;
-    pthread_create(&thread_reader, NULL, reader_thread, &args);
-    pthread_create(&thread_writer, NULL, writer_thread, &args);
-    
-    pthread_join(thread_reader, NULL);
-    pthread_join(thread_writer, NULL);
+    char buf[BUF_SIZE];
+    char enc[BUF_SIZE];
+
+    size_t n;
+
+    while ((n = fread(buf, 1, BUF_SIZE, src)) > 0) {
+        caesar(buf, enc, n);
+        fwrite(enc, 1, n, dst);
+    }
 
     fclose(src);
     fclose(dst);
-    destroy_queue(q);
 
-    if (!keep_running) {
-        unlink(dst_path);
-        printf("Операция прервана пользователем\n");
+    return 0;
+}
+
+void log_write(FILE* log, char* filename, int status) {
+    time_t now = time(NULL);
+
+    char timebuf[64];
+    strftime(timebuf, sizeof(timebuf), "%Y-%m-%d %H:%M:%S", localtime(&now));
+
+    fprintf(log,
+        "[%s] PID=%d TID=%lu FILE=%s STATUS=%s\n",
+        timebuf,
+        getpid(),
+        pthread_self(),
+        filename,
+        status == 0 ? "OK" : "ERR"
+    );
+
+    fflush(log);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 4) {
+        fprintf(stderr, "Usage: %s <src_files> <dst_dir_path> <key>\n", argv[0]);
         return 1;
     }
+
+    int total_files = argc - 3;
+    char** files = &argv[1];
+
+    char* out_dir = argv[argc - 2];
+    char* key = argv[argc - 1];
+
+    // signal(SIGINT, sigint_handler);
+
+    caesar_key(key[0]);
+
+    mkdir(out_dir, 0777);
+
+    FILE* log = fopen("log.txt", "a");
+
+    thread_args_t a = {
+        .filenames = files,
+        .total_files = total_files,
+        .next_file_index = 0,
+        .completed_files = 0,
+        .out_dir = out_dir,
+        .log = log
+    };
+
+    pthread_mutex_init(&a.file_index_mutex, NULL);
+    pthread_mutex_init(&a.counter_mutex, NULL);
+    pthread_mutex_init(&a.log_mutex, NULL);
+
+    pthread_t t[3];
+
+    for (int i = 0; i < 3; i++) {
+        pthread_create(&t[i], NULL, worker, &a);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        pthread_join(t[i], NULL);
+    }
+
+    printf("Processed files count: %d\n", a.completed_files);
+
+    fclose(log);
 
     return 0;
 }
--- secure_copy.h
+++ secure_copy.h
@@ -1,23 +1,31 @@
 #pragma once
 
 #define BUF_SIZE 4096
+#define TIMEOUT_SEC 5
 
-#include "queue.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <signal.h>
 #include <stdatomic.h>
 
 typedef struct {
-    char data[BUF_SIZE];
-    size_t len;
-    bool is_last;
-} buffer_t;
+    char** filenames;
+    int total_files;
 
-typedef struct {
-    FILE* src_file;
-    FILE* dst_file;
-    queue* q;
+    int next_file_index;
+    pthread_mutex_t file_index_mutex;
+
+    char* out_dir;
+
+    int completed_files;
+    pthread_mutex_t counter_mutex;
+
+    FILE* log;
+    pthread_mutex_t log_mutex;
 } thread_args_t;
 
-void* reader_thread(void* arg);
-void* writer_thread(void* arg);
+void* worker(void* arg);
+int process_file(char* filename, char* out_dir);
+void log_write(FILE* log, char* filename, int status);
