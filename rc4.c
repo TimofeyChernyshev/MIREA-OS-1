035dbacf577f5237c42f2c4e7e77cec107290600
Timofey Chernyshev <timkachern@mail.ru>
2026-05-24 23:29:22 +0300

    feat: make rc4 state secured

--- rc4.c
+++ rc4.c
@@ -1,6 +1,15 @@
 #include "rc4.h"
 
 #include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <errno.h>
+
+struct rc4_state {
+    unsigned char S[256];
+    int i;
+    int j;
+};
 
 static void swap(unsigned char* a, unsigned char* b) {
     unsigned char tmp = *a;
@@ -8,7 +17,15 @@ static void swap(unsigned char* a, unsigned char* b) {
     *b = tmp;
 }
 
-void rc4_init(rc4_state_t* state, const unsigned char* key, int key_len) {
+rc4_state_t* rc4_init(const unsigned char* key, int key_len) {
+    size_t size = sizeof(struct rc4_state);
+    
+    struct rc4_state* state = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_LOCKED, -1, 0);
+    if (state == MAP_FAILED) {
+        perror("mmap rc4 state");
+        return NULL;
+    }
+
     state->i = 0;
     state->j = 0;
 
@@ -23,21 +40,47 @@ void rc4_init(rc4_state_t* state, const unsigned char* key, int key_len) {
 
         swap(&state->S[i], &state->S[j]);
     }
+
+    if (mprotect(state, size, PROT_READ) == -1) {
+        perror("mprotect rc4 state");
+        munmap(state, size);
+        return NULL;
+    }
+
+    return state;
 }
 
-void rc4_crypt(rc4_state_t* state, unsigned char* data, size_t len) {
+void rc4_crypt(rc4_state_t* state_ptr, unsigned char* data, size_t len) {
+    struct rc4_state* state = (struct rc4_state*)state_ptr;
+    size_t size = sizeof(struct rc4_state);
+
+    mprotect(state, size, PROT_READ | PROT_WRITE);
+    
     for (size_t n = 0; n < len; n++) {
         state->i = (state->i + 1) & 0xFF;
         state->j = (state->j + state->S[state->i]) & 0xFF;
-
+        
         swap(&state->S[state->i], &state->S[state->j]);
-
+        
         unsigned char k = state->S[(state->S[state->i] + state->S[state->j]) & 0xFF];
-
         data[n] ^= k;
     }
+
+    mprotect(state, size, PROT_READ);
 }
 
-void rc4_cleanup(rc4_state_t* state) {
-    memset(state, 0, sizeof(rc4_state_t));
+void rc4_cleanup(rc4_state_t** state_ptr) {
+    if (!state_ptr || !*state_ptr) return;
+    
+    struct rc4_state* state = (struct rc4_state*)*state_ptr;
+    size_t size = sizeof(struct rc4_state);
+    
+    mprotect(state, size, PROT_READ | PROT_WRITE);
+    
+    memset(state, 0, size);
+    msync(state, size, MS_SYNC);
+    
+    munmap(state, size);
+    
+    *state_ptr = NULL;
 }
--- rc4.h
+++ rc4.h
@@ -3,16 +3,12 @@
 
 #include <stddef.h>
 
-typedef struct {
-    unsigned char S[256];
-    int i;
-    int j;
-} rc4_state_t;
+typedef struct rc4_state rc4_state_t;
 
-void rc4_init(rc4_state_t* state, const unsigned char* key, int key_len);
+rc4_state_t* rc4_init(const unsigned char* key, int key_len);
 
-void rc4_crypt(rc4_state_t* state, unsigned char* data, size_t len);
+void rc4_crypt(rc4_state_t* state_ptr, unsigned char* data, size_t len);
 
-void rc4_cleanup(rc4_state_t* state);
+void rc4_cleanup(rc4_state_t** state_ptr);
 
 #endif
